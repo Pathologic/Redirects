c8a97116734ba7c9035009f0a18e0db0f0dd6679
Pathologic <m@xim.name>
2024-12-16 19:50:51 +0300

    Update FiltersTrait.php

--- assets/modules/redirects/src/DocLister/FiltersTrait.php
+++ assets/modules/redirects/src/DocLister/FiltersTrait.php
@@ -9,9 +9,9 @@ trait FiltersTrait
         $where = [];
         $search = !empty($_POST['search']) && is_scalar($_POST['search']) ? $this->modx->db->escape(trim($_POST['search'])) : '';
         if($search) {
-            $where[] = "`source` LIKE '{$search}%'";
-            $where[] = "`target` LIKE '{$search}%'";
-            $where[] = "`description` LIKE '{$search}%'";
+            $where[] = "`source` LIKE '%{$search}%'";
+            $where[] = "`target` LIKE '%{$search}%'";
+            $where[] = "`description` LIKE '%{$search}%'";
         }
         if($where) {
             $config['addWhereList'] = implode(' OR ', $where);
