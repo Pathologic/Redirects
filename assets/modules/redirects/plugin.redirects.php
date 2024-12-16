7b98352eaae9af07d6eefe997369f26d07ca4089
Pathologic <m@xim.name>
2024-12-16 10:25:23 +0300

    Update plugin.redirects.php

--- assets/modules/redirects/plugin.redirects.php
+++ assets/modules/redirects/plugin.redirects.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'autoload.php';
+include_once MODX_BASE_PATH . 'assets/modules/redirects/autoload.php';
 $plugin = new Pathologic\Redirects\Plugin($modx, $params);
 $event = $modx->event->name;
 if (method_exists($plugin, $event)) {
